e8188b2a40fb49d2544a4e1dddca9b15bccf194b
Oinari-Shinto <oinari.shinto@gmail.com>
2021-11-05 23:53:57 +0300

    change component ShireInput v0.1

--- App.js
+++ App.js
@@ -18,14 +18,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       
-      <Text>{enteredShrine}</Text>
+      
       <StatusBar style="auto" />
-      <Button title="Change text" onPress={() => setEnteredShrine('The text changed!')}/>
-      <View style={styles.insideContainer}>
-        <TextInput placeholder="Shrine name" style={styles.input} onChangeText={shrineInputHandler} value={enteredShrine}/>
-        <Button title='ADD' onPress={addShrineHandler}/>
+      
+        
 
-      </View>
+      
       <FlatList data={courseShrines} renderItem={itemData => <ShrineItem title={itemData.item.value} />} />
         
       
@@ -40,16 +38,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  insideContainer: {
-    padding: 50,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1
-  },
+ 
   
 });
--- components/ShrineInput.js
+++ components/ShrineInput.js
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View, StyleSheet  } from "react-native";
 
-export const ShrineInput = () => {
+const ShrineInput = (props) => {
+    const [enteredShrine, setEnteredShrine] = useState('Japanese Shrine map');
+    //const [courseShrines, setCourseShrines] = useState([]);
+
+    const shrineInputHandler = (enteredText) => {
+        setEnteredShrine(enteredText);
+      };
+      //create here key in the object because FlatList need it
+      const addShrineHandler = () => {
+        setCourseShrines(currentShrines => [...currentShrines, { key: Math.random().toString(), value: enteredShrine}]);
+      };
     return (
-        <div>
-            
-        </div>
+        <View style={styles.container}>
+        <Text>{enteredShrine}</Text>
+        <Button title="Change text" onPress={() => setEnteredShrine('The text changed!')}/>
+        <View style={styles.insideContainer}>
+        <TextInput placeholder="Shrine name" style={styles.input} onChangeText={shrineInputHandler} value={enteredShrine}/>
+        <Button title='ADD' onPress={addShrineHandler}/>
+        </View>
+        </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    
+    insideContainer: {
+      padding: 50,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    input: {
+      width: '80%',
+      borderBottomColor: 'black',
+      borderBottomWidth: 1
+    },
+});
+export default ShrineInput;
