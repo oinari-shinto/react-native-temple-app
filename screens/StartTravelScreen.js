aa97f0bda3a3199eeffc4c0d1b37e32d9b076a9e
Oinari-Shinto <oinari.shinto@gmail.com>
2021-11-23 23:34:32 +0300

    prepare screen sections

---
+++ screens/StartTravelScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const StartTravelScreen = () => {
+    return (
+        <View>
+
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        
+    }
+
+});
+
+export default StartTravelScreen
