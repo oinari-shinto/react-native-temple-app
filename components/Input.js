eb6808415bdf47b892d687bfe81d1f017fc9229e
Oinari-Shinto <oinari.shinto@gmail.com>
2021-12-04 23:55:34 +0300

    Create Input component v0.1

---
+++ components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextInput,StyleSheet } from 'react-native'
+
+const Input = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Input;
