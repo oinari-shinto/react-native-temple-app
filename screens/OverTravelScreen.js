9865282d6babcaa84be6cf08887b7cc8308b3c52
Oinari-Shinto <oinari.shinto@gmail.com>
2022-01-10 22:46:01 +0300

    add Image style

--- screens/OverTravelScreen.js
+++ screens/OverTravelScreen.js
@@ -6,7 +6,12 @@ const OverTravelScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>The Travel is over!</Text>
-            <Image source={require('../assets/templeWall.png')}/>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/templeWall.png')} 
+                    style={styles.image}
+                    resizeMode='cover'/>
+            </View>
             <Text style={DefaultStyles.bodyText}>Number of temples: {props.templeNumber}</Text>
             <Text style={DefaultStyles.bodyText} >Number was: {props.userTemples}</Text>
             <Button title='NEW TRAVEL' onPress={props.onRestart}/>
@@ -20,6 +25,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        overflow: 'hidden',
+        borderColor: 'black',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 export default OverTravelScreen
