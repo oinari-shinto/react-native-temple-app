4336cc319e0b09ef344a766539398268a96cf179
Oinari-Shinto <oinari.shinto@gmail.com>
2022-01-22 21:48:14 +0300

    add Dimension API for listContainer

--- screens/TempleScreen.js
+++ screens/TempleScreen.js
@@ -1,5 +1,5 @@
 import React,  { useEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, ScrollView, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Card from '../components/Card';
@@ -92,13 +92,15 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        //marginTop: 20
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
     listContainer: {
         flex: 1,
-        width: '80%'
+        //width: '80%'
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
     },
     list: {
         flexGrow: 1,
