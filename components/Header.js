e92c66d2c005d2d9b75219ab2a9344703c11750d
Oinari-Shinto <oinari.shinto@gmail.com>
2022-02-01 23:58:12 +0300

    change header styles with Platform.select

--- components/Header.js
+++ components/Header.js
@@ -4,22 +4,32 @@ import Colors from '../constants/colors';
 
 const Header = (props) => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase, 
+            ...Platform.select({
+                ios: styles.headerIOS, 
+                android: styles.headerAndroid
+                })}}>
             <Text style={styles.headerTitle}>{props.title}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 16,
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white', 
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+    },
+    headerIOS: {
+        backgroundColor:'white', 
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary, 
     },
     headerTitle: {
         color: Platform.OS === 'ios' ? Colors.primary : 'white',
